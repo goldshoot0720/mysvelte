2c7f5ca31ac83262045b43a30b61804239403480
goldshoot0720 <goldshoot0720@gmail.com>
2025-04-30 21:36:43 +0800

    Spread props

---
+++ mysvelte git.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Spread props"
+git push -u origin master --force
+pause
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -100,6 +100,56 @@
 			showSub7 = false;
 		}
 	}
+	const subdata = [
+		{
+			subtitle: '訂閱',
+			textareavaluetitle1: '訂閱項目',
+			textareavaluetitle2: '訂閱費用',
+			textareavaluetitle3: '訂閱到期日',
+			textareavaluetitle4: '備註',
+			myangularsubtitle: 'myangularsub1'
+		},
+		{
+			subtitle: '學習',
+			textareavaluetitle1: '學習項目',
+			textareavaluetitle2: '學習進度',
+			textareavaluetitle3: '最近學習日',
+			textareavaluetitle4: '備註',
+			myangularsubtitle: 'myangularsub4'
+		},
+		{
+			subtitle: '門診處方箋',
+			textareavaluetitle1: '醫院/醫師',
+			textareavaluetitle2: '處方箋一',
+			textareavaluetitle3: '處方箋二',
+			textareavaluetitle4: '回診日期',
+			myangularsubtitle: 'myangularsub5'
+		},
+		{
+			subtitle: '消費',
+			textareavaluetitle1: '點數項目',
+			textareavaluetitle2: '剩餘點數',
+			textareavaluetitle3: '點數到期日',
+			textareavaluetitle4: '備註',
+			myangularsubtitle: 'myangularsub6'
+		},
+		{
+			subtitle: '日期時間',
+			textareavaluetitle1: '日期',
+			textareavaluetitle2: '日期資訊',
+			textareavaluetitle3: '手動倒數日期',
+			textareavaluetitle4: '手動換算倒數日期',
+			myangularsubtitle: 'myangularsub7'
+		},
+		{
+			subtitle: '動畫追劇',
+			textareavaluetitle1: '動畫追劇項目',
+			textareavaluetitle2: '當前集數',
+			textareavaluetitle3: '最近觀看日',
+			textareavaluetitle4: '備註',
+			myangularsubtitle: 'myangularsub9'
+		}
+	];
 </script>
 
 <svelte:head>
@@ -137,65 +187,23 @@
 </div>
 
 {#if showSub1}
-	<Sub
-		subtitle="訂閱"
-		textareavaluetitle1="訂閱項目"
-		textareavaluetitle2="訂閱費用"
-		textareavaluetitle3="訂閱到期日"
-		textareavaluetitle4="備註"
-		myangularsubtitle="myangularsub1"
-	/>
+	<Sub {...subdata[0]} />
 {/if}
 {#if showSub2}
 	<Sub2 />
 {/if}
 {#if showSub4}
-	<Sub
-		subtitle="學習"
-		textareavaluetitle1="學習項目"
-		textareavaluetitle2="學習進度"
-		textareavaluetitle3="最近學習日"
-		textareavaluetitle4="備註"
-		myangularsubtitle="myangularsub4"
-	/>
+<Sub {...subdata[1]} />
 {/if}
 {#if showSub5}
-	<Sub
-		subtitle="門診處方箋"
-		textareavaluetitle1="醫院/醫師"
-		textareavaluetitle2="處方箋一"
-		textareavaluetitle3="處方箋二"
-		textareavaluetitle4="回診日期"
-		myangularsubtitle="myangularsub5"
-	/>
+<Sub {...subdata[2]} />
 {/if}
 {#if showSub6}
-	<Sub
-		subtitle="消費"
-		textareavaluetitle1="點數項目"
-		textareavaluetitle2="剩餘點數"
-		textareavaluetitle3="點數到期日"
-		textareavaluetitle4="備註"
-		myangularsubtitle="myangularsub6"
-	/>
+<Sub {...subdata[3]} />
 {/if}
 {#if showSub7}
-	<Sub
-		subtitle="日期時間"
-		textareavaluetitle1="日期"
-		textareavaluetitle2="日期資訊"
-		textareavaluetitle3="手動倒數日期"
-		textareavaluetitle4="手動換算倒數日期"
-		myangularsubtitle="myangularsub7"
-	/>
+<Sub {...subdata[4]} />
 {/if}
 {#if showSub9}
-	<Sub
-		subtitle="動畫追劇"
-		textareavaluetitle1="動畫追劇項目"
-		textareavaluetitle2="當前集數"
-		textareavaluetitle3="最近觀看日"
-		textareavaluetitle4="備註"
-		myangularsubtitle="myangularsub9"
-	/>
+<Sub {...subdata[5]} />
 {/if}
